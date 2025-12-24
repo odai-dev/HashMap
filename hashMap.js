70bc319d98b8a00c37d7878d3687ceebbb9b71e4
Odai Jubran <95129948+odai-dev@users.noreply.github.com>
2025-12-25 00:28:01 +0300

    add remove method with linked list pointer logic

--- hashMap.js
+++ hashMap.js
@@ -76,4 +76,50 @@ class HashMap {
         }
         return false;
     }
+
+    remove(key) {
+        const index = this.hash(key);
+        
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+        }
+
+        if (this.buckets[index] == null) return false;
+        if (this.buckets[index].key === key) {
+            this.buckets[index] = this.buckets[index].next;
+            this.size--;
+            return true;
+        }
+
+        let prev = this.buckets[index];
+        let current = this.buckets[index].next;
+
+        while(current) {
+            if (current.key === key) {
+                prev.next = current.next;
+                this.size--;
+                return true;
+            }
+            prev = current;
+            current = current.next;
+        }
+
+        return false;
+    }
+
 }
+
+const myMap = new HashMap();
+myMap.set('apple', 'red');
+myMap.set('banana', 'yellow');
+
+console.log(myMap.get('apple')); 
+console.log(myMap.has('banana')); 
+console.log(myMap.has('cucumber')); 
+console.log(myMap.size); 
+
+myMap.set('apple', 'green');
+console.log(myMap.get('apple')); 
+console.log(myMap.size); 
+console.log(myMap.remove('apple'));
+console.log(myMap.remove('cucumber'));
